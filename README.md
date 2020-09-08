1ff408b34d8a13993370691f3485aa397c8d7021
manoj <manojhegde73@gmail.com>
2020-09-08 17:32:37 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# React-Dashboard
-Dashboard User interface developed using React.js
+
+### Available Scripts
+
+### `npm start`
+
+Runs the app in the development mode.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+
---
+++ src/routes.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+// Layout Types
+import { DefaultLayout } from "./layouts";
+
+// Route Views
+import BlogOverview from "./views/BlogOverview";
+import UserProfileLite from "./views/UserProfileLite";
+import AddNewPost from "./views/AddNewPost";
+import Tables from "./views/Tables";
+import BlogPosts from "./views/BlogPosts";
+
+export default [
+  {
+    path: "/",
+    exact: true,
+    layout: DefaultLayout,
+    component: () => <Redirect to="/blog-overview" />
+  },
+  {
+    path: "/blog-overview",
+    layout: DefaultLayout,
+    component: BlogOverview
+  },
+  {
+    path: "/user-profile-lite",
+    layout: DefaultLayout,
+    component: UserProfileLite
+  },
+  {
+    path: "/add-new-post",
+    layout: DefaultLayout,
+    component: AddNewPost
+  },
+  {
+    path: "/tables",
+    layout: DefaultLayout,
+    component: Tables
+  },
+  {
+    path: "/blog-posts",
+    layout: DefaultLayout,
+    component: BlogPosts
+  }
+];
